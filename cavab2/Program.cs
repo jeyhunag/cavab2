9bf59d08aa08b442341a37597b312345f8ccb095
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-29 01:12:20 +0300

    3

---
+++ cavab2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace cavab2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            namespace cavab2
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int number1, number2;
+                int Average;
+                Console.Write("Birinci ededinizi daxil edin: ");
+                number1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ikinci  ededinizi daxil edin: ");
+                number2 = Convert.ToInt32(Console.ReadLine());
+                Average = (number1 + number2) / 2;
+                Console.WriteLine("Ortalama:" + Average);
+            }
+        }
+    }
+
+}
+    }
+}
